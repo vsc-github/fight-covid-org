4d6685e21588903bb0c8d3ac8f1a99d44cd4c02c
vishwas <batty.wayn3@gmail.com>
2020-03-26 08:52:41 +0530

    Updates

---
+++ precache-manifest.ab70705b2ad8063909c57fb74b1e4dce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0015438d5386375c8de9e4e533df5f84",
+    "url": "/fight-covid/index.html"
+  },
+  {
+    "revision": "cf0c368d2a8d9b6c6a8c",
+    "url": "/fight-covid/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "add10eb15d6a650563a3",
+    "url": "/fight-covid/static/js/2.dc1b0f28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fight-covid/static/js/2.dc1b0f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf0c368d2a8d9b6c6a8c",
+    "url": "/fight-covid/static/js/main.c2e5d679.chunk.js"
+  },
+  {
+    "revision": "3c0b3d8ead867e44fb92",
+    "url": "/fight-covid/static/js/runtime-main.3706f4ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fight-covid/static/media/logo.5d5d9eef.svg"
+  }
+]);
