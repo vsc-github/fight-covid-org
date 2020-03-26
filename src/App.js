5bc739866b3cee6525f25ed3aae81fcbf3530e5f
vishwas <batty.wayn3@gmail.com>
2020-03-26 11:04:15 +0530

    add logo and title to navbar

--- src/App.js
+++ src/App.js
@@ -8,73 +8,22 @@ function App() {
   return (
     <div className="App">
 
-      <nav className="navbar is-transparent">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://versions.bulma.io/0.7.0">
-            <img src="https://versions.bulma.io/0.7.0/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
-          </a>
-          <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        <div id="navbarExampleTransparentExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item" href="https://versions.bulma.io/0.7.0/">
-              Home
-         </a>
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link" href="/documentation/overview/start/">
-                Docs
-            </a>
-              <div className="navbar-dropdown is-boxed">
-                <a className="navbar-item" href="/documentation/overview/start/">
-                  Overview
-               </a>
-                <a className="navbar-item" href="https://versions.bulma.io/0.7.0/documentation/modifiers/syntax/">
-                  Modifiers
-               </a>
-                <a className="navbar-item" href="https://versions.bulma.io/0.7.0/documentation/columns/basics/">
-                  Columns
-               </a>
-                <a className="navbar-item" href="https://versions.bulma.io/0.7.0/documentation/layout/container/">
-                  Layout
-               </a>
-                <a className="navbar-item" href="https://versions.bulma.io/0.7.0/documentation/form/general/">
-                  Form
-               </a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item" href="https://versions.bulma.io/0.7.0/documentation/elements/box/">
-                  Elements
-               </a>
-                <a className="navbar-item is-active" href="https://versions.bulma.io/0.7.0/documentation/components/breadcrumb/">
-                  Components
-               </a>
-              </div>
+      <nav class="navbar is-spaced">
+        <div class="container">
+          <div class="navbar-brand">
+            <div class="navbar-item">
+              <a href="/" className="logo">
+                <img class="image" src="https://res.cloudinary.com/diknntl0x/image/upload/v1585200205/fightcovid_n86cam.svg" alt="Page Speed monitoring" title="Web Performance Monitoring Service" />
+                <span>fightcovid.org</span>
+              </a>
             </div>
+            <div role="button" class="navbar-burger burger" tabindex="0"><span></span><span></span><span></span></div>
           </div>
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="field is-grouped">
-                <p className="control">
-                  <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
-                    <span className="icon">
-                      <i className="fab fa-twitter"></i>
-                    </span>
-                    <span>
-                      Tweet
-                  </span>
-                  </a>
-                </p>
-                <p className="control">
-                  <a className="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.6.2/bulma-0.6.2.zip">
-                    <span className="icon">
-                      <i className="fas fa-download"></i>
-                    </span>
-                    <span>Download</span>
-                  </a>
-                </p>
+          <div class="navbar-menu">
+            <div class="navbar-end">
+              <div class="navbar-item"><a class="navbar-item" href="/features">Features</a><a class="navbar-item" href="/pricing">Pricing</a><a class="navbar-item" title="Web Performance Blog" href="/web-performance-blog">Blog</a><a class="navbar-item" href="/contact">Contact</a></div>
+              <div class="navbar-item">
+                <div class="buttons"><a class="button is-light" href="/pricing">Get Started</a><a class="button is-primary" href="/sign-in">Log in</a></div>
               </div>
             </div>
           </div>
@@ -99,7 +48,7 @@ function App() {
 
           <div className="box">
 
-          <DetailsInput />
+            <DetailsInput />
 
           </div>
 
