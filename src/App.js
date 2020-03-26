727f88b097b001df10bc9650cf3a176382cc0068
vishwas <batty.wayn3@gmail.com>
2020-03-26 10:39:40 +0530

    remove inputs from main app.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import DetailsInput from './components/details-input'
+
 function App() {
   return (
     <div className="App">
@@ -97,76 +99,7 @@ function App() {
 
           <div className="box">
 
-            <form class="form-horizontal" >
-              <fieldset>
-
-
-                <legend>Personal Details</legend>
-
-
-                <div class="field">
-                  <label class="label" for="">Full name</label>
-                  <div class="control">
-                    <input id="" name="" type="text" placeholder="eg. Douglas Smith" class="input " required="" />
-
-                  </div>
-                </div>
-
-
-                <div class="field">
-                  <label class="label" for="email">Email address</label>
-                  <div class="control">
-                    <input id="email" name="email" type="text" placeholder="eg. youremail@gmail.com" class="input " />
-
-                  </div>
-                </div>
-
-
-                <div class="field">
-                  <label class="label" for="phone">Phone number</label>
-                  <div class="control">
-                    <input id="phone" name="phone" type="text" placeholder="eg. 8959076776" class="input " />
-
-                  </div>
-                </div>
-
-
-                <div class="field">
-                  <label class="label" for="city">City</label>
-                  <div class="control">
-                    <input id="city" name="city" type="text" placeholder="eg. current city of stay" class="input " />
-
-                  </div>
-                </div>
-
-
-                <div class="field">
-                  <label class="label" for="country">Country</label>
-                  <div class="control">
-                    <input id="country" name="country" type="text" placeholder="eg. USA, India, Italy" class="input " />
-
-                  </div>
-                </div>
-
-
-                <div class="field">
-                  <label class="label" for="zip">Zip Code</label>
-                  <div class="control">
-                    <input id="zip" name="zip" type="text" placeholder="eg. 90201" class="input " />
-
-                  </div>
-                </div>
-
-
-                <div class="field">
-                  <label class="label" for="address">Current Address</label>
-                  <div class="control">
-                    <textarea class="textarea" id="address" name="address"></textarea>
-                  </div>
-                </div>
-
-              </fieldset>
-            </form>
+          <DetailsInput />
 
           </div>
 
