7085482c93226728882a7e3abde8d7b567394d26
vishwas <batty.wayn3@gmail.com>
2020-03-30 22:27:35 +0530

    wire the route for /borrow and /donate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import Borrow from './components/borrow'
 
-function App() {
+export default function App() {
   return (
-    <Borrow />
-  )
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/borrow">
+            <Borrow />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <div>
+
+    <h2>Landing page here.</h2>
+
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/borrow">Borrow</Link>
+        </li>
+        <li>
+          <Link to="/donate">Donate</Link>
+        </li>
+      </ul>
+    </nav>
+
+  </div>;
+}
