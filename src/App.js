b0711ef8b378dff4e49b04608cf161efb0dc80cc
vishwas <batty.wayn3@gmail.com>
2020-04-04 21:40:35 +0530

    update homepage navigation

--- src/App.js
+++ src/App.js
@@ -39,15 +39,14 @@ function Home() {
         </div>
         
         <div className="gap"></div>
-        <Link href="/borrow" to="/borrow" className="link">Borrow</Link>
-        <div className="link">Learn</div>
-        <div className="link">Resources</div>
-        <div className="link button">CTA</div>
+        <Link href="/borrow" to="/borrow" className="link">GET HELP</Link>
+        <a href="#why-had" className="link">WHY HAD</a>
+        <a href="https://try.typeform.com/covid-19/" target="_blank" className="link button">DONATE</a>
       </nav>
 
       <div className="banner">
         <div className="title">Get help to overcome financial stress during COVID19</div>
-        <Link to="/borrow" className="cta">Borrow</Link>
+        <Link to="/borrow" className="cta">Get Help</Link>
       </div>
       
 
@@ -56,19 +55,23 @@ function Home() {
     <section className="mission">
       <div className="title">Our misson</div>
 
-      <div className="description">Our misson is to ensure everyone has enough to eat during quarantine/lockdown.</div>
+      <div className="description">Our mission is to make sure everyone has shelter, food and medications during a disaster</div>
 
       <div className="bottom"></div>
     </section>
 
     <section className="how">
-      <div className="title">How it works</div>
+      <div className="title">REGISTER TO GET HELP</div>
+
+      <div className="description">
+      100% of all donation are given to those in need in Real-Time. Donations are never held by HAD.
+      </div>
 
       <div className="tiles">
         <div className="tile">
           <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1585981466/create_rfelwk.svg" />
           <div className="heading">Create an account</div>
-          <div className="sub">How have you been affected?</div>
+          <div className="sub">Tell us how you have been affected. </div>
         
         </div>
         <div className="tile">
@@ -89,8 +92,12 @@ function Home() {
       <div className="bottom"></div>
     </section>
 
-    <section className="mission">
-      <div className="title">Why now?</div>
+    <section id="why-had">
+      <div className="title">COVID-19 GLOBAL PANDEMIC</div>
+
+      <div className="description">
+      The Coronavirus has disrupted millions of people in almost every industry. Together so we can all make it through this pandemic.
+      </div>
 
       <div className="gallery">
         <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1585982215/group_ddk02j.png" />
