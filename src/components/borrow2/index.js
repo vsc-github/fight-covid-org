e5d236f229b886c1f74caa2980d20c2c092270c1
vishwas <batty.wayn3@gmail.com>
2020-04-06 22:51:56 +0530

    add education and financial section

--- src/components/borrow2/index.js
+++ src/components/borrow2/index.js
@@ -130,6 +130,352 @@ const Borrow2 = () => (
                 </div>
                 <div className="right">
 
+                  <div className="top"> Already signed up? <span>Sign in</span></div>
+
+                  <div className="box">
+
+                    <legend>Educational Details</legend>
+
+
+                    <fieldset>
+
+
+                      <div class="field">
+                        <label class="label" for="">Are you currently a student?</label>
+                        <div class="control">
+                          <label class="radio inline" for="student-0">
+                            <input type="radio" name="student" id="student-0" value="Yes" checked="checked" />
+                            Yes
+</label>
+                          <label class="radio inline" for="student-1">
+                            <input type="radio" name="student" id="student-1" value="No" />
+                            No
+</label>
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                        <label class="label" for="college">Highest level of education</label>
+                        <div class="control">
+                          <input id="college" name="college" type="text" placeholder="eg. HSC, graduate, post graduate, etc" class="input " />
+
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                        <label class="label" for="field">Field of study</label>
+                        <div class="control">
+                          <input id="field" name="field" type="text" placeholder="eg. farming, technology, foods and services etc" class="input " />
+
+                        </div>
+                      </div>
+
+                      <div class="field">
+                        <label class="label" for="field">Licences and Certificates</label>
+                        <div class="control">
+                          <input id="certificates" name="certificates" type="text" placeholder="eg. project management, solutions architect  etc" class="input " />
+
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                        <label class="label" for="skills">Please enter as many skills you have</label>
+                        <div class="control">
+                          <input id="skills" name="skills" type="text" placeholder="eg. cooking, cleaning, driving, accounting, typing etc" class="input " />
+
+                        </div>
+                      </div>
+
+                    </fieldset>
+
+                    <button onClick={() => fullpageApi.moveSectionDown()}>
+                      Next
+                </button>
+
+                  </div>
+
+
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+
+          <div className="section">
+            <div className="section">
+
+              <div className="wrap">
+                <div className="left">
+
+                  <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1586181919/hands_ie7jj9.jpg" className="bg" />
+                  <div className="layer">
+
+                    <a href="/" className="logo">
+                      <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1585982215/logo_kp9op5.svg" />
+                    </a>
+
+                    <div className="quote">
+
+                      HAD is a non-profit that helps out when you are in need. 100% of the donations go to the field.
+      </div>
+
+                  </div>
+                </div>
+                <div className="right">
+
+
+                  <div className="top"> Already signed up? <span>Sign in</span></div>
+
+                  <div className="box">
+
+                    <fieldset>
+
+
+                      <legend>Empolyment Details</legend>
+
+
+                      <div class="field">
+                        <label class="label" for="jobtitle">Job Title</label>
+                        <div class="control">
+                          <input id="jobtitle" name="jobtitle" type="text" class="input " />
+
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                        <label class="label" for="jobtype">Employment Type (Full Time, Part Time, Casual/Hourly)</label>
+                        <div class="control">
+                          <input id="jobtype" name="jobtype" type="text" class="input " />
+
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                        <label class="label" for="company">Company</label>
+                        <div class="control">
+                          <input id="company" name="company" type="text" class="input " />
+
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                        <label class="label" for="affect">Did Covid-19 affect your employment?</label>
+                        <div class="control">
+                          <label class="radio" for="previously-0">
+                            <input type="radio" name="affect" id="affect-0" value="Yes" checked="checked" />
+                            Yes
+</label>
+                          <label class="radio" for="previously-1">
+                            <input type="radio" name="affect" id="affect-1" value="No" />
+                            No
+</label>
+                        </div>
+                      </div>
+
+                      <div class="field">
+                        <label class="label" for="previously">Will your job come back after Covid-19?</label>
+                        <div class="control">
+                          <label class="radio" for="previously-0">
+                            <input type="radio" name="previously" id="previously-0" value="Yes" checked="checked" />
+                            Yes
+</label>
+                          <label class="radio" for="previously-1">
+                            <input type="radio" name="previously" id="previously-1" value="No" />
+                            No
+</label>
+                          <label class="radio" for="previously-1">
+                            <input type="radio" name="previously" id="previously-1" value="No" />
+                            Maybe
+</label>
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                        <label class="label" for="remote">Are you open for remote work?</label>
+                        <div class="control">
+                          <label class="radio" for="remote-0">
+                            <input type="radio" name="remote" id="remote-0" value="Yes" checked="checked" />
+                            Yes
+</label>
+                          <label class="radio" for="remote-1">
+                            <input type="radio" name="remote" id="remote-1" value="No" />
+                            No
+</label>
+                        </div>
+                      </div>
+
+                    </fieldset>
+
+                    <button onClick={() => fullpageApi.moveSectionDown()}>
+                      Next
+                </button>
+
+                  </div>
+
+
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+
+          <div className="section">
+            <div className="section">
+
+              <div className="wrap">
+                <div className="left">
+
+                  <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1586181919/hands_ie7jj9.jpg" className="bg" />
+                  <div className="layer">
+
+                    <a href="/" className="logo">
+                      <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1585982215/logo_kp9op5.svg" />
+                    </a>
+
+                    <div className="quote">
+
+                      HAD is a non-profit that helps out when you are in need. 100% of the donations go to the field.
+      </div>
+
+                  </div>
+                </div>
+                <div className="right">
+
+                  <p>Section 1 (welcome to fullpage.js)</p>
+                  <button onClick={() => fullpageApi.moveSectionDown()}>
+                    Click me to move down
+    </button>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+
+          <div className="section">
+            <div className="section">
+
+              <div className="wrap">
+                <div className="left">
+
+                  <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1586181919/hands_ie7jj9.jpg" className="bg" />
+                  <div className="layer">
+
+                    <a href="/" className="logo">
+                      <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1585982215/logo_kp9op5.svg" />
+                    </a>
+
+                    <div className="quote">
+
+                      HAD is a non-profit that helps out when you are in need. 100% of the donations go to the field.
+      </div>
+
+                  </div>
+                </div>
+                <div className="right">
+
+
+                  <div className="top"> Already signed up? <span>Sign in</span></div>
+
+                  <div className="box">
+
+                    <fieldset>
+
+
+                      <legend>Financial Details</legend>
+
+
+                      <div class="field">
+                        <label class="label" for="cashapp">Paypal</label>
+                        <div class="control">
+                          <input id="paypal" name="paypal" type="text" placeholder="freddie@gmail.com" class="input " />
+
+                        </div>
+                      </div>
+
+                      <div class="field">
+                        <label class="label" for="cashapp">Cashapp</label>
+                        <div class="control">
+                          <input id="cashapp" name="cashapp" type="text" placeholder="$mary123" class="input " />
+
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                        <label class="label" for="venmo">Venmo</label>
+                        <div class="control">
+                          <input id="venmo" name="venmo" type="text" placeholder="eg. @markvenmo" class="input " />
+
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                        <label class="label" for="upi">UPI</label>
+                        <div class="control">
+                          <input id="upi" name="upi" type="text" placeholder="eg. sandy@ybl" class="input " />
+
+                        </div>
+                      </div>
+
+
+
+                      <div class="field">
+                        <label class="label" for="payment">Any other financial details</label>
+                        <div class="control">
+                          <textarea class="textarea" id="payment" name="payment"></textarea>
+                        </div>
+                      </div>
+
+                    </fieldset>
+
+                    <button onClick={() => fullpageApi.moveSectionDown()}>
+                      Next
+                </button>
+
+
+                  </div>
+
+
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+
+          <div className="section">
+            <div className="section">
+
+              <div className="wrap">
+                <div className="left">
+
+                  <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1586181919/hands_ie7jj9.jpg" className="bg" />
+                  <div className="layer">
+
+                    <a href="/" className="logo">
+                      <img src="https://res.cloudinary.com/diknntl0x/image/upload/v1585982215/logo_kp9op5.svg" />
+                    </a>
+
+                    <div className="quote">
+
+                      HAD is a non-profit that helps out when you are in need. 100% of the donations go to the field.
+      </div>
+
+                  </div>
+                </div>
+                <div className="right">
+
                   <p>Section 1 (welcome to fullpage.js)</p>
                   <button onClick={() => fullpageApi.moveSectionDown()}>
                     Click me to move down
